44adcb32f93b3603f599b346a7d282b11149d36a
Gaston Lopez <gastonlopez436@gmail.com>
2024-08-26 16:25:24 -0300

    Logica 1/7

---
+++ IgualdadValores.js
@@ -0,0 +1,34 @@
+let numeroUno = 1;
+let stringUno = "1";
+let numeroTreinta = 30;
+let stringTreinta = "30";
+let numeroDiez = 10;
+let stringDiez = "10";
+
+if (numeroUno == stringUno) {
+  console.log(
+    "Las variables numeroUno y stringUno tienen el mismo valor, pero tipos diferentes.\n"
+  );
+} else {
+  console.log("Las variables numeroUno y stringUno no tienen el mismo valor.\n");
+}
+
+if (numeroTreinta === stringTreinta) {
+  console.log(
+    "Las Variables numeroTreinta y stringTreinta tienen el mismo valor y el mismo tipo.\n"
+  );
+} else {
+  console.log(
+    "Las Variables numeroTreinta y stringTreinta no tienen el mismo tipo.\n"
+  );
+}
+
+if (numeroDiez == stringDiez) {
+  console.log(
+    "Las variables numeroDiez y stringDiez tienen el mismo valor, pero tipos diferentes."
+  );
+} else {
+  console.log(
+    "Las variables numeroDiez y stringDiez no tienen el mismo valor."
+  );
+}
